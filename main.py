3ddc6c683ad8db56334c896fea2e6ebbc7a692a4
ukxxx <42536544+ukxxx@users.noreply.github.com>
2023-06-30 17:15:27 +0300

    Initial commit

--- main.py
+++ main.py
@@ -4,54 +4,44 @@ import fake_headers
 import lxml
 import json
 
-LINK = r"https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
+LINK = r'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
 result = []
 
-
 def get_html(url):
-    headers = fake_headers.Headers(headers=True, os="win", browser="chrome").generate()
+    headers = fake_headers.Headers(headers=True, os='win', browser='chrome').generate()
     response = requests.get(url, headers=headers)
     return response.text
 
 
+
 def parse_data(html):
-    soup = bs4.BeautifulSoup(html, "lxml")
-    # print(soup)
-    vacancies = soup.find_all("div", class_="serp-item")
-    # print(vacancies)
+    soup = bs4.BeautifulSoup(html, 'lxml')
+    vacancies = soup.find_all('div', class_='serp-item')
     for vacancy in vacancies:
-        title = vacancy.find("a").text
-        # print(title)
+        title = vacancy.find('a').text
         try:
-            salary = vacancy.find("span", class_="bloko-header-section-3").text
+            salary = vacancy.find('span', class_='bloko-header-section-3').text
         except:
-            salary = "Зарплата не указана"
-        link_to_vacancy = vacancy.find("a").get("href")
-        # print(link_to_vacancy)
-        # print(salary)
-        company = vacancy.find("a", class_="bloko-link bloko-link_kind-tertiary").text
-        # print(company)
-        city = vacancy.find(
-            "div", {"data-qa": "vacancy-serp__vacancy-address", "class": "bloko-text"}
-        ).text
-        # print(city)
-        result.append(
-            {
-                "title": title,
-                "link_to_vacancy": link_to_vacancy,
-                "salary": salary,
-                "company": company,
-                "city": city,
-            }
-        )
+            salary = 'Зарплата не указана'
+        link_to_vacancy = vacancy.find('a').get('href')
+
+        company = vacancy.find('a', class_='bloko-link bloko-link_kind-tertiary').text
+
+        city = vacancy.find('div', {'data-qa': 'vacancy-serp__vacancy-address', 'class': 'bloko-text'}).text
+
+        result.append({
+            'title': title,
+            'link_to_vacancy': link_to_vacancy,
+            'salary': salary,
+            'company': company,
+            'city': city
+            })
     return result
 
-
 def main():
-    with open("data.json", "w", encoding="utf-8") as file:
+    with open('data.json', 'w', encoding='utf-8') as file:
         json.dump(parse_data(get_html(LINK)), file, indent=4, ensure_ascii=False)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-    print("Готово!\n")
+    print('Готово!\n')
